32e9ff4a8c090ca58b6dd4656cdbec02623a662e
kpatenio <katherinepatenio@gmail.com>
2018-10-04 08:51:28 -0600

    Update background.js

--- background.js
+++ background.js
@@ -2,7 +2,7 @@
 // note that permission to use storage API is used via PERMISSIONS in manifest
 chrome.runtime.onInstalled.addListener(function(){
   chrome.storage.sync.set({running:"true"}, function(){
-    console.log("QUOI is running: true");
+    console.log("QUOI is currently installed");
   });
   // TODO: set up context menus following this example from Google:
   // https://github.com/GoogleChrome/chrome-app-samples/blob/master/samples/context-menu/main.js

9b610a36a0a8f7788aad6dcc03591f8e03cd6d40
kpatenio <katherinepatenio@gmail.com>
2018-10-29 01:38:52 -0600

    first commit to v1-popupmenu

--- background.js
+++ background.js
@@ -2,8 +2,8 @@
 // note that permission to use storage API is used via PERMISSIONS in manifest
 chrome.runtime.onInstalled.addListener(function(){
   chrome.storage.sync.set({running:"true"}, function(){
-    console.log("QUOI is currently installed");
-  });
+    console.log("QUOI is currently installed and running");
+  }); //TODO: see if this is necessary (ex. do we need a variable init upon installation?)
   // TODO: set up context menus following this example from Google:
   // https://github.com/GoogleChrome/chrome-app-samples/blob/master/samples/context-menu/main.js
 });

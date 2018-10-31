ba0a43e92723d0a76f112a6c1badedae031b5c8b
kpatenio <katherinepatenio@gmail.com>
2018-10-31 03:28:40 -0600

    workaround context menu using new window

--- background.js
+++ background.js
@@ -1,34 +1,62 @@
 /* According to documentation: https://developer.chrome.com/extensions/contextMenus */
 // selection context = highlighting something
 // page context = anywhere on the page
-var context_menu = {
-  "id": "QUOI_widget",
+
+/*
+Apparently, it is not possible to open up the popup window on the browser from the contexts
+menu (this is a design choice made by Google).
+
+Sources:
+ https://stackoverflow.com/questions/17851700/how-to-open-the-default-popup-from-context-menu-in-a-chrome-extension
+ https://stackoverflow.com/questions/33361715/open-extension-popup-when-click-on-context-menu
+
+
+ Alternate workaround - make a new window
+ Content Scripts with chrome api: https://developer.chrome.com/extensions/content_scripts
+ */
+
+var contextId = "quoiContextId";
+
+var contextMenu = {
+  "id": contextId,
   "title": "QUOI",
   "contexts": ["selection", "page"]
-}
+};
 
-chrome.contextMenus.create(context_menu);
+// create context menu
+chrome.contextMenus.create(contextMenu);
 
 // see if our context item is selected
 chrome.contextMenus.onClicked.addListener(function(clicked){
-  // if highlighted a word
-  if (clicked.menuItemid == "QUOI_widget" && clicked.selectionText){
-    // TODO - WONT WORK! // https://stackoverflow.com/questions/17851700/how-to-open-the-default-popup-from-context-menu-in-a-chrome-extension
-    //https://stackoverflow.com/questions/33361715/open-extension-popup-when-click-on-context-menu
-    console.log("CLICKED");
-    chrome.tabs.create({
-      "url": chrome.extension.getURL('popup.html'),
-      "active": false
-  }, function(tab) {
-      // After the tab has been created, open a window to inject the tab
-      chrome.windows.create({
-          "tabId": "TSET",
-          "type": 'popup',
-          "focused": true
-      });
-  });
-  }
-})
 
+  // if highlighted a word
+  if (clicked.menuItemId == contextId && clicked.selectionText){
+    chrome.windows.create ({
+      url : "popup.html",
+      focused : false,
+      top: 200,
+      left: 700,
+      height: 200,
+      width: 400,
+      type : "popup"
+    });
+  }
+
+  // else anywhere on the page
+  else if (clicked.menuItemId == "QUOI_widget" && clicked.selectionText) {
+    chrome.windows.create ({
+      url : "popup.html",
+      focused : false,
+      top: 200,
+      left: 700,
+      height: 200,
+      width: 400,
+      type : "popup"
+    });
+  }
+
+}); // end of listener
+
+// Interesting way to make a small window (not popup): https://stackoverflow.com/a/11699421
 
 // TODO: use set popup : https://stackoverflow.com/a/29415742
